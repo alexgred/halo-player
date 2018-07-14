ff4e841ed08f9f547cb48fb60eeae474ebad65a8
Alex Dolzhenkov <alexgred.dev@gmail.com>
2018-07-14 03:15:09 +0300

    Default config files

---
+++ src/ts/config/defaults.ts
@@ -0,0 +1,10 @@
+import Config from '../interfaces/Config';
+
+const defaults: Config = {
+  classes: {
+    container: 'halo_container',
+    video: 'halo_video'
+  }
+};
+
+export default defaults;
